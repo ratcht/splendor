280755112da4f969aca2050f06c76b2696c7d0a8
Ali <alialhamadani72@gmail.com>
2026-06-09 00:58:46 -0700

    feat: add random strategy

---
+++ game/strategy/rand.py
@@ -0,0 +1,18 @@
+import random
+from state import TableState
+from actions import Action, ACTION_CLASSES
+
+
+# ── strategy ──────────────────────────────────────────────────────────────────
+
+class RandomStrategy:
+  def choose_action(self, state: TableState) -> Action:
+    board  = state.board
+    player = state.players[state.current]
+
+    # pick action class
+    for action_class in random.sample(ACTION_CLASSES, len(ACTION_CLASSES)):
+      if avail_actions := action_class.legal_actions(board, player):
+        return random.choice(avail_actions)
+
+    raise RuntimeError("No legal actions available")
