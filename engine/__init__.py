f2c4da91231628f3a60e2052a25477294ebdb0d8
Ali A <alialhamadani72@gmail.com>
2026-09-06 15:43:25 -0700

    feat: expose actions and models in engine api

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,15 +1,36 @@
+from .actions import (
+  GEM_LIMIT,
+  NON_GOLD_GEMS,
+  Action,
+  BuyCard,
+  ReserveCard,
+  TakeThreeGems,
+  TakeTwoGems,
+  legal_actions,
+)
 from .engine import run_game, take_turn
-from .models import Gem, GemStack
+from .models import LEVELS, Card, Gem, GemStack, Noble
 from .state import BoardState, PlayerState, TableState
 from .table import Table
 
 __all__ = [
+  "GEM_LIMIT",
+  "LEVELS",
+  "NON_GOLD_GEMS",
+  "Action",
   "BoardState",
+  "BuyCard",
+  "Card",
   "Gem",
   "GemStack",
+  "Noble",
   "PlayerState",
+  "ReserveCard",
   "Table",
   "TableState",
+  "TakeThreeGems",
+  "TakeTwoGems",
+  "legal_actions",
   "run_game",
   "take_turn",
 ]
